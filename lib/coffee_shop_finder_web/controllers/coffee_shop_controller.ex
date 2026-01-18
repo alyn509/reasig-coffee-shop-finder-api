20bfb7984dd031d55e29c988ba72475266ba41a5
Alin Cruceat <alyn_509@yahoo.com>
2026-01-18 14:04:14 +0200

    cleaner api params checks and errors

--- lib/coffee_shop_finder_web/controllers/coffee_shop_controller.ex
+++ lib/coffee_shop_finder_web/controllers/coffee_shop_controller.ex
@@ -3,21 +3,43 @@ defmodule CoffeeShopFinderWeb.CoffeeShopController do
 
   alias CoffeeShopFinder.{Data.DataStore, Geo.NearestShopsFinder, Geo.CoordinateValidator}
 
-  def nearest(conn, %{"x" => x, "y" => y}) do
-    with {:ok, x} <- CoordinateValidator.parse(x),
-         {:ok, y} <- CoordinateValidator.parse(y) do
-      shops = DataStore.all()
+  @doc """
+  Returns nearest coffee shops to the given coordinates.
+  Expects query params `x` and `y`.
+  """
+  def nearest(conn, params) do
+    case parse_coords(params) do
+      {:ok, x, y} ->
+        run_nearest(conn, x, y)
 
-      result =
-        shops
-        |> NearestShopsFinder.find(x, y)
-
-      json(conn, %{results: result})
-    else
-      _ ->
+      {:error, :missing_params} ->
         conn
         |> put_status(:bad_request)
-        |> json(%{error: "Invalid coordinates"})
+        |> json(%{error: "Missing required query parameters: x and y"})
+
+      {:error, :invalid_coords} ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "Coordinates are invalid or out of range"})
     end
   end
+
+  # Private helpers
+
+  defp run_nearest(conn, x, y) do
+    shops = DataStore.all()
+    result = NearestShopsFinder.find(shops, x, y)
+    json(conn, %{results: result})
+  end
+
+  defp parse_coords(%{"x" => x, "y" => y}) do
+    with {:ok, x_val} <- CoordinateValidator.parse(x),
+         {:ok, y_val} <- CoordinateValidator.parse(y) do
+      {:ok, x_val, y_val}
+    else
+      _ -> {:error, :invalid_coords}
+    end
+  end
+
+  defp parse_coords(_), do: {:error, :missing_params}
 end
